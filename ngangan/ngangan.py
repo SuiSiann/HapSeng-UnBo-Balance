3b9eff5e4870c42c6364e31aae3f473d2755da02
Omah <omah@ithuan.tw>
2022-05-11 10:31:05 +0800

    mikilim to wen a codad

---
+++ ngangan/ngangan.py
@@ -0,0 +1,20 @@
+import csv
+
+with open(
+        '名姓字表 - 字姓.csv') as tong:
+        reader = csv.reader(tong)
+        next(reader, None)
+        senn_dict = {}
+        for pit in reader:
+                print(pit[2])
+
+
+
+with open(
+        '名姓字表 - 人名.csv') as tong:
+        reader = csv.reader(tong)
+        next(reader, None)
+        senn_dict = {}
+        for pit in reader:
+            if '(文)' in pit[1]:
+                print(pit[:2])

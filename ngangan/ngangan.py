cf262f04984035285673f3ee0490928d246b4baa
Omah <omah@ithuan.tw>
2022-05-11 10:47:54 +0800

    patongal to pay

--- ngangan/ngangan.py
+++ ngangan/ngangan.py
@@ -20,3 +20,16 @@ with open(
             if '(文)' in pit[1]:
                 ngangan.append(pit[0])
         print(ngangan)
+
+
+
+with open(
+        '名姓字表 - 人名.csv') as tong:
+        reader = csv.reader(tong)
+        next(reader, None)
+        senn_dict = {}
+        ngangan = []
+        for pit in reader:
+            if '(白)' in pit[1]:
+                ngangan.append(pit[0])
+        print(ngangan)

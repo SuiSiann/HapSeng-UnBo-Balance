cff6f779694c7501538c1b36b5a2efe2e2dc67a9
Omah <omah@ithuan.tw>
2022-05-11 11:06:42 +0800

    salof to sapatongal to popyaw

--- ngangan/ngangan.py
+++ ngangan/ngangan.py
@@ -20,9 +20,7 @@ with open(
         for pit in reader:
             if '(文)' in pit[1]:
                 ngangan_w.append(pit[0])
-            if '(白)' in pit[1]:
-                ngangan_p.append(pit[0])
-            if '(不標)' in pit[1]:
+            if '(白)' in pit[1] or '(不標)' in pit[1]:
                 ngangan_p.append(pit[0])
         print(ngangan_w, ngangan_p)
 

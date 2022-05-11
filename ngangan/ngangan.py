0e1ba05cfeb9e75313dd4f63df2472f8adc870dc
Omah <omah@ithuan.tw>
2022-05-11 14:11:43 +0800

    sanga' to ngangan

--- ngangan/ngangan.py
+++ ngangan/ngangan.py
@@ -25,7 +25,14 @@ with open(
 
 
 for index in range(0, 400, 2):
-    print(senn[index], 'X', ngangan_w[index*2])
-    print(senn[1+index], ngangan_w[1+index*2], 'X')
+    print(senn[index % 100], 'X', ngangan_w[index*2])
+    print(senn[(1+index) % 100], ngangan_w[1+index*2], 'X')
     print('X', ngangan_w[2+index*2])
     print(ngangan_w[3+index*2], 'X')
+
+fout = open('ngangan.txt', 'w')
+for index in range(0, 400, 2):
+    fout.write(senn[index % 100] + 'X' + ngangan_w[index*2] + '\n')
+    fout.write(senn[(1+index) % 100] + ngangan_w[1+index*2] + 'X'+ '\n')
+    fout.write('X' + ngangan_w[2+index*2]+ '\n')
+    fout.write(ngangan_w[3+index*2] + 'X'+ '\n')
